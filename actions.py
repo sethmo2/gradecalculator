ed2232bf26bf2a6ec7fdbe05fc6bfc78f63531dd
sethmo2 <sethmoberholtzer@gmail.com>
2024-01-10 14:19:00 -0600

    Fixed case where error was thrown when nothing initially in JSON file

--- actions.py
+++ actions.py
@@ -1,35 +1,5 @@
 import json
-
-# class Course:
-#     def __init__(self, name, hours, grade):
-#         self.name = name
-#         self.hours = hours
-#         self.grade = grade
-#     def __string__(self):
-#         return f"{self.name} {self.hours} {self.grade}"
-
-def add_course():
-    course_list = []
-    course_list = get_courses_from_file(course_list)
-    display_courses(course_list)
-    while True:
-        name = input("Enter Course Name: ")
-        hours = input("Credit Hours: ")
-        grade = input("Grade: ")
-        course = {"name": name, "hours": hours, "grade": grade}
-        course_list.append(course)
-        status = input("Type yes if done: ")
-        if status in ['yes']:
-            break
-    write_courses_to_file(course_list)
-    display_courses(course_list)
         
-# def read_course_data():
-#     with open("course_data.json", "r") as file:
-#         course_info_string = file.read()
-#         course_info_list = json.loads(course_info_string)
-#         print(course_info_list["name"])
-
 def get_courses_from_file(course_list):
     with open("course_data.json", "r") as file:
         course_list = json.load(file)
@@ -42,3 +12,22 @@ def write_courses_to_file(course_list):
 def display_courses(course_list):
     for course in course_list:
         print(f"{course['name']:8} {course['hours']:1} {course['grade']:2}")
+
+def add_course():
+    course_list = []
+    try:
+        course_list = get_courses_from_file(course_list)
+    except:
+        print("No courses!")
+    display_courses(course_list)
+    while True:
+        name = input("Enter Course Name: ")
+        hours = input("Credit Hours: ")
+        grade = input("Grade: ")
+        course = {"name": name, "hours": hours, "grade": grade}
+        course_list.append(course)
+        status = input("Type yes if done: ")
+        if status in ['yes']:
+            break
+    write_courses_to_file(course_list)
+    display_courses(course_list)
